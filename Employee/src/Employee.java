babee44487d12604ab03a7fe4ef077c51202a819
suphanatchanlek <suphanatchanlek@gmail.com>
2025-02-12 15:39:28 +0700

    add Set attribute values ​​with methods

--- Employee/src/Employee.java
+++ Employee/src/Employee.java
@@ -1,7 +1,25 @@
 public class Employee {
     // Attribute
-    // ปิดข้อมูลเอาไว้
     private String id;
     private String name;
     private double salary;
+
+    // Create Methods
+    public void setId(String id) {
+        // keyword this คือการอ้างอิงคุณสมบัติของแต่ละตัว
+        this.id = id;
+    }
+    public  void setName(String name){
+        this.name = name;
+    }
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    // สร้าง Methods ที่เอาไว้แสดง print
+    public void displayEmployee(){
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + salary);
+    }
 }
--- Employee/src/Main.java
+++ Employee/src/Main.java
@@ -3,5 +3,27 @@ public class Main {
         // ให้สร้าง Object
         // ชื่อClass ชื่อObject = new ชื่อClass();
         Employee e1 = new Employee(); //Create Object
+
+        /*
+        กำหนดค่า set แบบโดยตรง
+        e1.id = "1"; // Assign
+        e1.name = "Suphanat Chanlek";
+        e1.salary = 30000.0;
+
+         */
+
+        // เรียกใช้งานผ่าน Methods ที่เราสร้างไว้ใน Class
+        e1.setId("1");
+        e1.setName("Suphanat Chanlek1");
+        e1.setSalary(30000);
+
+        Employee e2 = new Employee();
+        e2.setId("2");
+        e2.setName("Suphanat Chanlek2");
+        e2.setSalary(35000);
+
+        // เรียกใช้ methods ที่เราสร้างไว้ใน Employee
+        e1.displayEmployee();
+        e2.displayEmployee();
     }
 }
